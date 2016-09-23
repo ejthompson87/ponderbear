8cfbdabb1201a2bb98af056f44710f33689d1e51
Emma Thompson <ejthompson87@gmail.com>
2016-09-23 13:44:24 -0700

    Updated admin controller
    - now includes check admin logged in
    - database includes field for is_admin

--- adminController.js
+++ adminController.js
@@ -2,27 +2,35 @@
 //when admin submit button pressed
 exports.createAdminController = function (db) {
     return function (req,res) {
-        
         // check to make sure admin user
-        // if (req.session.userid == 17)
-
-        // link id and to request
-        if (req.body.inputAnswer != null && req.body.inputAnswer !== "") {
-            db.query("UPDATE idea_requests SET answer = ? WHERE id = ?", [req.body.inputAnswer, req.body.id], function(err, results) {
-                if (err) {
-                    console.log(err);
-                    res.renderWithLayout('admin', {adminInputErr : "Error submitting answer"});
+        db.query('SELECT * FROM users WHERE id = ?', [req.session.userid], function(err, results) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (results[0].is_admin === 1) {
+                // link id and to request
+                if (req.body.inputAnswer != null && req.body.inputAnswer !== "") {
+                    db.query("UPDATE idea_requests SET answer = ? WHERE id = ?", [req.body.inputAnswer, req.body.id], function(err, results) {
+                        if (err) {
+                            console.log(err);
+                            res.renderWithLayout('admin', {adminInputErr : "Error submitting answer"});
+                        }
+                        // if open request, redirect to admin and display requests
+                        if (results.affectedRows > 0) {
+                            res.redirect('/admin');
+                        } else { 
+                            res.renderWithLayout('admin', {adminInputErr : "Error submitting answer"});
+                        }
+                    });
+                } else {
+                    // bad request error message for submitting empty string 
+                    res.send(400);
                 }
-                // if open request, redirect to admin and display requests
-                if (results.affectedRows > 0) {
-                    res.redirect('/admin');
-                } else { 
-                    res.renderWithLayout('admin', {adminInputErr : "Error submitting answer"});
-                }
-            });
-        } else {
-            // bad request error message for submitting empty string 
-            res.send(400);
-        }
+            } else {
+                console.log(err);
+                return;
+            }
+        }); 
     };
 };
