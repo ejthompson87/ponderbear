acb442de4baa91f24a4d32e85ac12b149818d91b
Emma Thompson <ejthompson87@gmail.com>
2017-04-05 10:36:19 +0100

    Made changes to style - added SASS - cleaned up style

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
+
+
+
+
+
+
+
+
 Local Development
 =================
 
 Run MySQL:
 * Totally new container, new DB:
-  * `docker run --name mySql -e MYSQL_ROOT_PASSWORD=monopoly -p 3306:3306 -d mysql:latest`
+  * `docker run --name mySql -e MYSQL_ROOT_PASSWORD=USER-PASSWORD -p 3306:3306 -d mysql:latest`
 
 Run PHPMyAdmin:
 * Run the docker container, linking it to the MySql container:
   * `docker run --name myadmin -d --link mySql:db -p 8080:80 phpmyadmin/phpmyadmin`
+
+
--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.use(session({
     saveUninitialized: true
 }));
 
-
 // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -72,24 +71,14 @@ app.get('/admin', function(req,res){
             }
             // check to make sure admin user
             if (results[0].is_admin === 1) {
-                // dbPool.query('SELECT * FROM idea_requests WHERE answer IS NULL', function(err, results) {
-                // dbPool.query('SELECT * FROM idea_requests INNER JOIN users on idea_requests.user_id = users.id WHERE answer IS NULL', function(err, results) {
-
                dbPool.query('SELECT idea_requests.*, username FROM idea_requests INNER JOIN users on idea_requests.user_id = users.id WHERE idea_requests.answer IS NULL', function(err, results) {
                     if (err) {
                         console.log(err);
                         res.renderWithLayout('admin', {adminErr : "Error retrieving request"});
                     } else { 
                         console.log(results);
-                    //    dbPool.query('SELECT username FROM users WHERE id = ?', [results[0].user_id], function(err, resultsU) {
-                    //        if (err) {
-                    //            console.log(err);
-                    //            return;
-                    //        } else {
                         res.renderWithLayout('admin', {questions : results});  
                     }
-                    //    });
-                    //}
                 });
             } else {
                 res.renderWithLayout('admin', {restrictedAlert : "Restricted Access Only"});
